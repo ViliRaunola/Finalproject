f6d7ea461ea687663a05df731c77ef14d75e6dbf
ViliRaunola <raunolavili@gmail.com>
2020-04-24 18:35:11 +0300

    Lisäsin onNavigation selectionin

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,9 +9,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Part of onNavigationItemSelected.
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar, R.string.navigatio_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -34,5 +44,34 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
+    //This handles what item has been selected by Vili.
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_uni:
+                Toast.makeText(this, "LUT", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_uni2:
+                Toast.makeText(this, "Aalto", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_uni3:
+                Toast.makeText(this, "Tampere", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_rev:
+                Toast.makeText(this, "Your reviews", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_acc:
+                Toast.makeText(this, "Your Account", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Your Have Logged Out", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        //After the item has been selected closes the side menu.
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
 }
