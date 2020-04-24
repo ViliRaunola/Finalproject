374505d62d968a09900851cbcea239d8da2d803b
ViliRaunola <raunolavili@gmail.com>
2020-04-24 20:43:29 +0300

    Spinner toimii Lut_fragmentissa

--- app/src/main/java/com/example/finalproject/Lut_fragment.java
+++ app/src/main/java/com/example/finalproject/Lut_fragment.java
@@ -4,18 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class Lut_fragment extends Fragment {
+
+    public ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+    Restaurant laser = new Restaurant("LASER");
+    Restaurant buffet = new Restaurant("BUFFETTI SAATANA");
+
     private Spinner restaurant_spinner;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.lut, container, false);
+        restaurants.add(laser);
+        restaurants.add(buffet);
         restaurant_spinner = (Spinner)v.findViewById(R.id.restaurant_spinner);
+        ArrayAdapter<Restaurant> arrayAdapter = new ArrayAdapter<Restaurant>(getActivity(), android.R.layout.simple_spinner_item, restaurants);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        restaurant_spinner.setAdapter(arrayAdapter);
+
+
+
+
 
         return v;
     }
---
+++ app/src/main/java/com/example/finalproject/Restaurant.java
@@ -0,0 +1,19 @@
+package com.example.finalproject;
+
+public class Restaurant {
+
+    private String name;
+
+    public Restaurant(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        String temp;
+        temp = this.name;
+        return temp;
+    }
+
+
+}
