670b10d66b5f57fe11b0344bb386105affa194e6
AlmondRumble11 <54431988+AlmondRumble11@users.noreply.github.com>
2020-04-24 19:55:43 +0300

    Spinner otettu

--- app/src/main/java/com/example/finalproject/Lut_fragment.java
+++ app/src/main/java/com/example/finalproject/Lut_fragment.java
@@ -4,16 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Lut_fragment extends Fragment {
-
+    private Spinner restaurant_spinner;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.lut, container, false);
+        View v = inflater.inflate(R.layout.lut, container, false);
+        restaurant_spinner = (Spinner)v.findViewById(R.id.restaurant_spinner);
+
+        return v;
     }
 }
