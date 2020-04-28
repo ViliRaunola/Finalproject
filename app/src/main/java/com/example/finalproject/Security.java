6552c8bfb4db4d0c9a082809f1cc822d0ef0571e
TaneliM <64307246+TaneliM@users.noreply.github.com>
2020-04-28 14:32:42 +0300

    Added comments to "Security" class

--- app/src/main/java/com/example/finalproject/Security.java
+++ app/src/main/java/com/example/finalproject/Security.java
@@ -4,15 +4,23 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 
-public class Security {
+/* Has a method to generate "securedPassword"
+from "originalPassword" and "salt" using "sha-512"
+and a method to generate "salt" byte[] */
 
+public class Security {
     public static String getSecuredPassword(String originalPassword, byte[] salt) {
         String securedPassword = null;
 
         try {
+            // Creates MessageDigest instance for "SHA-512"
             MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+            // Adds salt bytes to "md"
             md.update(salt);
             byte[] bytes = md.digest(originalPassword.getBytes());
+
+            // Converts byte[] to string "securedPassword" in hexadecimal using StringBuilder()
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < bytes.length; i++) {
                 sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
@@ -24,9 +32,11 @@ public class Security {
         return securedPassword;
     }
 
+    // Uses SecureRandom to generate random bytes to byte[] "salt", returns salt
     public static byte[] getSalt() {
         byte[] salt = new byte[16];
         try {
+
             SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
             sr.nextBytes(salt);
         } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
