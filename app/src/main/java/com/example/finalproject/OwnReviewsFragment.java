48ca770ed73feb384a41b62d7e103cd9c4f34c84
AlmondRumble11 <54431988+AlmondRumble11@users.noreply.github.com>
2020-04-28 18:59:29 +0300

    own reviews activity listview onclicklsitener added

--- app/src/main/java/com/example/finalproject/OwnReviewsFragment.java
+++ app/src/main/java/com/example/finalproject/OwnReviewsFragment.java
@@ -1,12 +1,17 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ActionMenuView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +27,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OwnReviewsFragment extends Fragment {
+    private PopupWindow popUp;
+    private LinearLayout layout;
+    private TextView tv;
     private Spinner sortingSpinner;
     private List<String> sortingList = new ArrayList<String>();
     private List<String> reviewsList = new ArrayList<String>();
@@ -44,9 +52,9 @@ public class OwnReviewsFragment extends Fragment {
         sortingList.add("Average Score");
 
         //test list
-        reviewsList.add("Food:  Lihapullat ja perunamuusi\nDate:  28.4.2020\nRestaurant:  Laseri\nAverage Score:  4.5");
-        reviewsList.add("Food:  Lohipullat ja perunamuusi\nDate:  29.4.2020\nRestaurant:  Lut Buffet\nAverage Score:  4.0");
-
+        reviewsList.add("Food:  Lihapullat ja perunamuusi\nDate:  28.4.2020\nRestaurant:  Laseri\nAverage Score:  4.5 stars");
+        reviewsList.add("Food:  Lohipullat ja perunamuusi\nDate:  29.4.2020\nRestaurant:  Lut Buffet\nAverage Score:  4.0 stars");
+        reviewsList.add("Food:  Lohipullat ja perunamuusi\nDate:  29.4.2020\nRestaurant:  Lut Buffet\nAverage Score:  4.0 stars");
         //arrayadapter for spinner
         ArrayAdapter<String> ap = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, sortingList);
         ap.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -58,19 +66,24 @@ public class OwnReviewsFragment extends Fragment {
         notPublishedReviews.setAdapter(ap2);
 
 
+
         //sorting spinner listener
         sortingSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                selectedSortingMethod = sortingSpinner.getSelectedItem().toString();
-               if (selectedSortingMethod.equals("Date")){
-                   Toast.makeText(getContext(), "Sorting by date", Toast.LENGTH_SHORT).show();
-               }else if (selectedSortingMethod.equals("Food")){
-                   Toast.makeText(getContext(), "Sorting by food", Toast.LENGTH_SHORT).show();
-               }else if (selectedSortingMethod.equals("Restaurant")){
-                   Toast.makeText(getContext(), "Sorting by restaurant", Toast.LENGTH_SHORT).show();
-               }else if (selectedSortingMethod.equals("Average Score")){
-                   Toast.makeText(getContext(), "Sorting by average score", Toast.LENGTH_SHORT).show();
+               switch (selectedSortingMethod) {
+                   case "Date":
+                       Toast.makeText(getContext(), "Sorting by date", Toast.LENGTH_SHORT).show();
+                       break;
+                   case "Food":
+                       Toast.makeText(getContext(), "Sorting by food", Toast.LENGTH_SHORT).show();
+                       break;
+                   case "Restaurant":
+                       Toast.makeText(getContext(), "Sorting by restaurant", Toast.LENGTH_SHORT).show();
+                       break;
+                   case "Average Score":
+                       Toast.makeText(getContext(), "Sorting by average score", Toast.LENGTH_SHORT).show();
                }
             }
 
@@ -79,7 +92,25 @@ public class OwnReviewsFragment extends Fragment {
 
             }
         });
+
+
+        publishedReviews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        notPublishedReviews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getActivity(),AccountActivity.class);
+                startActivity(intent);
+            }
+        });
         return v;
     }
+
+
 
 }
