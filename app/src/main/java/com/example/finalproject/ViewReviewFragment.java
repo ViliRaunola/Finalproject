4b1e96599ddee84b5d0996c431bae9eae74eee66
ViliRaunola <raunolavili@gmail.com>
2020-05-05 13:55:00 +0300

    Added hide review button for admin.

--- app/src/main/java/com/example/finalproject/ViewReviewFragment.java
+++ app/src/main/java/com/example/finalproject/ViewReviewFragment.java
@@ -22,6 +22,7 @@ public class ViewReviewFragment extends Fragment {
     private TextView foodInfoWindow;
     private TextView writtenReview;
     private Button removeButton;
+    private Button hideButton;
 
     private Bundle informationBundle;
     private FoodReview selectedFoodReview;
@@ -40,6 +41,7 @@ public class ViewReviewFragment extends Fragment {
         foodInfoWindow = (TextView)v.findViewById(R.id.foodInfoWindow_viewReview);
         writtenReview = (TextView)v.findViewById(R.id.writtenReview_viewReview);
         removeButton = (Button)v.findViewById(R.id.deleteReviewButton_viewReview);
+        hideButton = (Button)v.findViewById(R.id.hideReviewButton_viewReview);
 
 
         try{
@@ -57,13 +59,29 @@ public class ViewReviewFragment extends Fragment {
 
         //Makes the button disabled if user is not an admin user
         removeButton.setEnabled(user.getIsAdminUser());
+        hideButton.setEnabled(user.getIsAdminUser());
 
+        //Checks if the current is user. If true it shows the buttons and makes them functional
         if(user.getIsAdminUser()){
             removeButton.setVisibility(View.VISIBLE);
+            hideButton.setVisibility(View.VISIBLE);
         }else {
             removeButton.setVisibility(View.INVISIBLE);
+            hideButton.setVisibility(View.INVISIBLE);
         }
 
+        //When this button is pressed the selected review's "published"-boolean is set to false.
+        //Writes the review back to file with changed boolean
+        hideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedFoodReview.setPublished(false);
+                parseClass.parseRestaurantReviews(selectedFoodReview.getRestaurant(), getContext());
+                parseClass.modifyRestaurantReviewXmlFile(getContext(), selectedFoodReview);
+                Toast.makeText(getContext(),"You have hid this review",Toast.LENGTH_SHORT).show();
+                getActivity().onBackPressed();
+            }
+        });
 
 
         //When pressed "DELETE-BUTTON" removes the review that the admin is viewing
