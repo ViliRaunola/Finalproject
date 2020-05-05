e210c41d7b1e455ca0d183f5ba879b2e0506e5b8
AlmondRumble11 <54431988+AlmondRumble11@users.noreply.github.com>
2020-05-05 21:55:35 +0300

    settings fragment commented and cleaned up the code

--- app/src/main/java/com/example/finalproject/SettingsFragment.java
+++ app/src/main/java/com/example/finalproject/SettingsFragment.java
@@ -10,54 +10,55 @@ import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 public class SettingsFragment extends Fragment {
     private Spinner languageSpinner;
     private Button changeLanguageButton;
     private int position;
     private List<String> languageList = new ArrayList<String>();
-
     ParseClass parseClass = ParseClass.getInstance();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.settings, container, false);
+
         changeLanguageButton = (Button)v.findViewById(R.id.changeLanguageButton_settings);
         languageSpinner = (Spinner)v.findViewById(R.id.languageSpinner_settings);
+
+        //reading language xml and getting list for available languages
         languageList = parseClass.parseLanguage(getActivity(), languageList);
-        final ArrayAdapter<String> languageSpinnerAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, languageList);
+
+        //setting adapter for language spinner using language list
+        ArrayAdapter<String> languageSpinnerAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, languageList);
         languageSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         languageSpinner.setAdapter(languageSpinnerAdapter);
 
+        //setting language spinner position to current app language
         if (Locale.getDefault().getLanguage().equals("en")){
             languageSpinner.setSelection(0);
         }else if (Locale.getDefault().getLanguage().equals("fi")) {
             languageSpinner.setSelection(1);
         }
 
+        //changing language based on position
         changeLanguageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //getting current spinner position
                 position = languageSpinner.getSelectedItemPosition();
+
+                //setting new language and recreating the activity
+                //https://www.youtube.com/watch?v=zILw5eV9QBQ source for changing the language
                 switch (position) {
                     case 0:
                         Language.getInstance().setLocale("en", getContext());
                         getActivity().recreate();
                         break;
+
                     case 1:
                         Language.getInstance().setLocale("fi", getContext());
                         getActivity().recreate();
@@ -65,7 +66,6 @@ public class SettingsFragment extends Fragment {
                 }
             }
         });
-        System.out.println(Locale.getDefault().getLanguage());
         return v;
     }
 
